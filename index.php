7072a0d0402a25f67ffb9b1383a3f4c136c4bada
shadoheart <s.rimbert@hotmail.com>
2017-01-10 09:55:20 +0100

    création fichier index

--- index.php
+++ index.php
@@ -1,1 +1,43 @@
 <?php
+class Model
+{
+    public $string;
+
+    public function __construct()
+    {
+        $this -> $string = "Le MVC, c'est le pied !";
+    }
+}
+
+class View
+{
+    private $model;
+    private $controller;
+
+    public function __construct($controller, $model)
+    {
+        $this -> controller = $controller;
+        $this -> model = $model;
+    }
+
+    public function output()
+    {
+        return "<p>" . $this -> model -> string . "<p>";
+    }
+}
+
+class Controller
+{
+    private $model;
+
+    public function __construct($model)
+    {
+        $this -> model = $model;
+    }
+}
+
+$model = new model();
+$controller = new controller($model);
+$view = new view($controller, $model);
+
+echo $view -> output();
